40514acda9c94f3a81704fde66ed6daf4b34d4b4
spoolae <ohotnic147feed@gmail.com>
2022-05-16 13:16:12 +0300

    Day 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,30 @@
-(function() {
-  "use strict";
+// var app = new Vue({
+//     el: "app",
+//     data:{
+    //   products: [
+    //     {id:1, title:"Product 1", short_text:"Short text 1", image:"", desc:"Full decs"},
+    //     {id:2, title:"Product 2", short_text:"Short text 2", image:"", desc:"Full decs"}
+    //   ]
+//     },
+//     mounted: function() {
+//       console.log(Window.localStorage.getItem('product'));
+//     },
+//     methods: {
+//       addItem: function(id){
+//         Window.localStorage.setItem('product', id);
+//       }
+//     }
+//   });
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
+new Vue ({
+    el:"#app",
+    data:{
+        products: [
+            {id:1, title:"Bordeaux 237", short_text:"Varieties and hybrids of beets, in all their diversity, allow you to get vegetables of different shapes and sizes, the desired ripening period", image:"1.jpg"},
+            {id:2, title:"Modana", short_text:"Varieties and hybrids of beets, in all their diversity, allow you to get vegetables of different shapes and sizes, the desired ripening period", image:"2.jpg"},
+            {id:3, title:"Pablo F1", short_text:"Varieties and hybrids of beets, in all their diversity, allow you to get vegetables of different shapes and sizes, the desired ripening period", image:"3.jpg"},
+            {id:4, title:"Bohemia", short_text:"Varieties and hybrids of beets, in all their diversity, allow you to get vegetables of different shapes and sizes, the desired ripening period", image:"4.jpg"},
+            {id:5, title:"Detroit", short_text:"Varieties and hybrids of beets, in all their diversity, allow you to get vegetables of different shapes and sizes, the desired ripening period", image:"5.jpg"},
+          ]
     }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-})()
+});
